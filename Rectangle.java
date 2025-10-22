b981fd208ef47d2c0392b2e148c5171c09f2c93a
Krish Patel <krishpatel8976@gmail.com>
2025-10-22 13:51:05 -0400

    feat: added consrtuctor for rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,10 @@
 public class Rectangle {
-    
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
 }
