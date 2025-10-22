5025c0fd989c2a18bc933617bab2d8edf805556e
arshdippamma <187570030+arshdippamma@users.noreply.github.com>
2025-10-22 14:04:45 -0400

    feat: create Trapezoid class with constructor, getPerimeter, and getArea methods

---
+++ Trapezoid.java
@@ -0,0 +1,24 @@
+
+public class Trapezoid {
+	double long_base;
+	double short_base;
+	double side_1;
+	double side_2;
+	double height;
+	
+	public Trapezoid(double long_base, double short_base, double side_1, double side_2, double height) {
+		this.long_base = long_base;
+		this.short_base = short_base;
+		this.side_1 = side_1;
+		this.side_2 = side_2;
+		this.height = height;
+	}
+	
+	public double getPerimeter() {
+		return this.long_base + this.short_base + this.side_1 + this.side_2;
+	}
+	
+	public double getArea() {
+		return ((this.long_base + this.short_base) * h) / 2;
+	}
+}
